3fa973cd743d4a7ca5c6dc0b1eaefb1412e88467
kosonogov123 <122813241+kosonogov123@users.noreply.github.com>
2023-01-26 03:32:20 +0300

    задание1

---
+++ задача1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу которая принимает число и выдает его квадрат
+Console.Clear();
+Console.WriteLine("Напишите число, чтобы получить значение его квадрата.");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат: {n*n} ");
+
