3cb346bddcd11579c7481ecbf7e3bf587bb5c658
kosonogov123 <122813241+kosonogov123@users.noreply.github.com>
2023-01-26 03:07:11 +0300

    урок версия 1

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // console.Clear();
+// // Console.WriteLine("Hello, World!");
+
+
+// // int n = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("введите 1-ое число");
+// int n = int.Parse(Console.ReadLine());
+// Console.WriteLine("введите 2-ое число");
+// int m = int.Parse(Console.ReadLine());
+// if (n>m)
+// Console.WriteLine(n);
+// else if (n<m)
+// Console.WriteLine(m);
+// else 
+// Console.WriteLine("=");
+// //  если double то ToDouble
+// // Console.WriteLine($"Результат: {n}");
+
+
