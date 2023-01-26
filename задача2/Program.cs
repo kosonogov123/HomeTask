ade3290eabedcacd956fffd0648baabd810afe3f
kosonogov123 <122813241+kosonogov123@users.noreply.github.com>
2023-01-26 03:55:42 +0300

    задача2

---
+++ задача2/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимаем 2 числа и проверяем, является ли первое квадратом второго
+
+Console.Clear();
+Console.WriteLine("Напишите два числа, программа проверит, является ли первое квадратом второго.");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n == m*m)
+Console.WriteLine("является");
+else
+Console.WriteLine("Не является");
