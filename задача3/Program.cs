7f5b03d0947efd26c5962ee4b238e625b14786c4
kosonogov123 <122813241+kosonogov123@users.noreply.github.com>
2023-01-26 04:03:45 +0300

    задача3

---
+++ задача3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите номер дня недели, программа выдаст его название");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1)
+Console.WriteLine("понедельник");
+if (n == 2)
+Console.WriteLine("вторник");
+if (n == 3)
+Console.WriteLine("среда");
+if (n == 4)
+Console.WriteLine("четверг");
+if (n == 5)
+Console.WriteLine("пятница");
+if (n == 6)
+Console.WriteLine("суббота");
+if (n == 7)
+Console.WriteLine("воскресенье");
+else if (n > 7)
+Console.WriteLine("Вы ввели слишком большое число, попробуйте от 1 до 7");
